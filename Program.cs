7c492aed73a6b4f302aa58312cf2ff044704d84b
MichalKulpa <104196838+MichalKulpa@users.noreply.github.com>
2022-05-14 12:56:57 +0200

    E08 solved

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            // TODO: add your solution...
+            int[] numbers = { 2, 5, 1, 2, 5 };
+            RemoveDuplicates(numbers);
+        }
+        static void RemoveDuplicates(int[] numbers)
+        {
+            for(int i = 0;i<numbers.Length;i++)
+            {
+                for(int j=i+1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j]) numbers[j] = -1;
+
+                }
+                if (numbers[i] >= 0) Console.WriteLine(numbers[i]);
+            }
+                       
         }
     }
 }

1db9fe0d75582e0d84e634929ab945eaf8736296
MichalKulpa <104196838+MichalKulpa@users.noreply.github.com>
2022-05-14 10:54:08 +0200

    E04 solved

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            // TODO: add your solution...
+            DrawTriangle(3, '*');
+          
+        }
+        static void DrawTriangle(int size, char character)
+        {
+            for(int i =0; i < size; i++)
+            {
+                Console.Write(character);
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(character);
+                }
+                Console.WriteLine();
+            }
+            for (int i = size; i > 1; i--)
+            {
+                for(int j = i; j > 1; j--)
+                {
+                    Console.Write(character);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }

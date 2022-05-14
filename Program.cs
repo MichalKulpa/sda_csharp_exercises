1ea49233c96b7cdd5ab79beb6b9584130f4d8504
MichalKulpa <104196838+MichalKulpa@users.noreply.github.com>
2022-05-14 12:15:38 +0200

    E07 solved

--- Program.cs
+++ Program.cs
@@ -6,7 +6,27 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            // TODO: add your solution...
+            string word = "ElitaSorbet";
+            Console.WriteLine(Speak1337(word));
+        }
+        static string Speak1337(string word)
+        {
+            char[] leetLetters = { '4', '3', '1', '0', '$' };
+            char[] naturalLetters = { 'a', 'e', 'i', 'o', 's' };
+            word = word.ToLower();
+            char[] characters = word.ToCharArray();
+            for (int i = 0; i < characters.Length; i++)
+            {
+                for (int j = 0; j < naturalLetters.Length; j++)
+                {
+                    if (characters[i] == naturalLetters[j])
+                    {
+                        characters[i] = leetLetters[j];
+                    }
+                }
+            }
+            string newWord = new string(characters);
+            return newWord;
         }
     }
 }

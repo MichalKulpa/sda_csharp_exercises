52d93546c048e59ad3158e1d0ad43ce735dcd126
MichalKulpa <104196838+MichalKulpa@users.noreply.github.com>
2022-05-14 15:56:52 +0200

    E10 optimized

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace sda_csharp_exercises
             int temporary=0;
             for (int i = 0; i < array.Length - 1; i++)
             {
-                for (int j = 0; j < array.Length - 1; j++)
+                for (int j = 0; j < array.Length -i - 1; j++)
                 {
                     if (array[j] > array[j + 1])
                     {

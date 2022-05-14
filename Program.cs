7d3f393175af99cbad9ccbc6d657b85f4f41ca0f
MichalKulpa <104196838+MichalKulpa@users.noreply.github.com>
2022-05-14 10:13:41 +0200

    E01 solved

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            // TODO: add your solution...
+            try
+            {
+                Console.WriteLine("Podaj cenę produktu:");
+                int price = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Podaj liczbę produktów:");
+                int quantity = Convert.ToInt32(Console.ReadLine());
+                PriceList(price, quantity);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void PriceList(int price, int quantity)
+        {
+            for (int i = 1; i <= quantity; i++)
+            {
+                Console.WriteLine($"Liczba sztuk: {i}, łączna cena {i * price} zł.");
+            }
         }
     }
 }

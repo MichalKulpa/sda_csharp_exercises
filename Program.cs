23a41b49a42f3c5825f66234aacd83b2711ec6dd
MichalKulpa <104196838+MichalKulpa@users.noreply.github.com>
2022-05-14 12:33:28 +0200

    E07 Solved and done

--- Program.cs
+++ Program.cs
@@ -7,7 +7,10 @@ namespace sda_csharp_exercises
         static void Main(string[] args)
         {
             string word = "ElitaSorbet";
-            Console.WriteLine(Speak1337(word));
+            string leetWord = Speak1337(word);
+            Console.WriteLine(leetWord);
+            Console.WriteLine(Speak1337Decoder(leetWord));
+
         }
         static string Speak1337(string word)
         {
@@ -22,6 +25,25 @@ namespace sda_csharp_exercises
                     if (characters[i] == naturalLetters[j])
                     {
                         characters[i] = leetLetters[j];
+                    }
+                }
+            }
+            string newWord = new string(characters);
+            return newWord;
+        }
+        static string Speak1337Decoder(string word)
+        {
+            char[] leetLetters = { '4', '3', '1', '0', '$' };
+            char[] naturalLetters = { 'a', 'e', 'i', 'o', 's' };
+            word = word.ToLower();
+            char[] characters = word.ToCharArray();
+            for (int i = 0; i < characters.Length; i++)
+            {
+                for (int j = 0; j < leetLetters.Length; j++)
+                {
+                    if (characters[i] == leetLetters[j])
+                    {
+                        characters[i] = naturalLetters[j];
                     }
                 }
             }

655978db487d76215c02e13749f064ef32a31d79
MichalKulpa <104196838+MichalKulpa@users.noreply.github.com>
2022-05-14 15:51:28 +0200

    E10 solved

--- Program.cs
+++ Program.cs
@@ -6,7 +6,35 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            // TODO: add your solution...
+            int[] array = { 5, 2, 8, 1, 0, 3 };
+            PrintArray(BubbleSort(array));
+        }
+        static void PrintArray(int[] array)
+        {
+            foreach(int i in array)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+        }
+        static int[] BubbleSort(int[] array)
+        {
+            int temporary=0;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+
+                        temporary = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temporary;
+                    }
+                }
+            }
+
+            return array;
         }
     }
 }

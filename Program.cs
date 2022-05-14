0037b6d28f6d312263ce1a6d969d324bfeed0d10
MichalKulpa <104196838+MichalKulpa@users.noreply.github.com>
2022-05-14 14:50:08 +0200

    E09 solved

--- Program.cs
+++ Program.cs
@@ -6,7 +6,41 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            // TODO: add your solution...
+            string pesel = "55030101193";
+            Console.WriteLine(PeselIsCorrect(pesel));
+
         }
+        static int SumOfPeselProducts(int[] pesel)
+        {
+            int sum=0;
+            int[] imprtances = {1,3,7,9,1,3,7,9,1,3,1 };
+            for(int i = 0; i < pesel.Length-1; i++)
+            {
+                sum  +=pesel[i] * imprtances[i];
+            }
+            return sum;
+        }
+        static int[] PeselToIntArray(string pesel)
+        {
+            int[] peselIntArray=new int [11];
+            char[] peselCharArray = pesel.ToCharArray();
+            for (int i = 0; i < peselCharArray.Length; i++)
+            {
+                peselIntArray[i] = Convert.ToInt32(peselCharArray[i]);
+            }
+            return peselIntArray;
+        }
+        static int PeselModulo(int sum)
+        {
+            return sum % 10;
+        }
+        static bool PeselIsCorrect(string pesel)
+        {
+            int[] peselArray = PeselToIntArray(pesel);
+            int sum = SumOfPeselProducts(peselArray);
+            return PeselModulo(sum) == 0;
+        }
+
+
     }
 }
